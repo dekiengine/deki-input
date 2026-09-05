08d287e8fbd234dc645fce6d074ceface89c19aa
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-05 10:30:39 +0200

    Docs for deki-input from deki-editor 46ff166

--- navtreedata.js
+++ navtreedata.js
@@ -28,7 +28,7 @@ var NAVTREE =
     [ "Components", "index.html#autotoc_md0", null ],
     [ "Feature flags", "index.html#autotoc_md1", null ],
     [ "Component reference", "index.html#autotoc_md2", [
-      [ "InputCollider (Input Collider)", "index.html#c-inputcollider", null ]
+      [ "InputCollider", "index.html#c-inputcollider", null ]
     ] ]
   ] ]
 ];
